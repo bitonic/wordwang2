c57d81817d55b89ee00ab5e52757577dbadccaa2
Francesco Mazzoli <f@mazzo.li>
2013-10-24 21:25:42 +0100

    Remove monad

--- server/src/WordWang.hs
+++ server/src/WordWang.hs
@@ -1,14 +1,7 @@
 module WordWang
     ( module WordWang.Objects
     , module WordWang.Messages
-    , module WordWang.Monad
-    , receive
     ) where
 
 import           WordWang.Objects
 import           WordWang.Messages
-import           WordWang.Monad
-
-receive :: WW Resp
-receive = undefined
-
