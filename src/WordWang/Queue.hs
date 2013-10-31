bc6cf75e4eec73eff28872023303b3dab1a9142e
Francesco Mazzoli <f@mazzo.li>
2013-11-01 00:25:11 +0100

    use Chan for the time being, my Queue seems broken

--- src/WordWang/Queue.hs
+++ src/WordWang/Queue.hs
@@ -1,28 +1,19 @@
-module WordWang.Queue where
+-- TODO write something faster than a Chan
+module WordWang.Queue (Queue, newQueue, writeQueue, flushQueue) where
 
-import           Control.Applicative ((<*>))
-import           Control.Monad (void)
 import           Data.Functor ((<$>))
+import           Control.Concurrent.Chan (Chan, newChan, writeChan, readChan)
 
-import           Control.Concurrent.MVar (MVar, newMVar, newEmptyMVar, modifyMVar_, tryPutMVar, withMVar, takeMVar, modifyMVar)
 
-data Queue a = Queue
-    { queueQueue  :: MVar [a]
-    , queueSignal :: MVar ()
-    , queueLock   :: MVar ()
-    }
+newtype Queue a = Queue (Chan a)
 
 -- | Build and returns a new instance of 'Queue'
 newQueue :: IO (Queue a)
-newQueue = Queue <$> newMVar [] <*> newEmptyMVar <*> newMVar ()
+newQueue = Queue <$> newChan
 
 -- | Write a value to a 'Queue'.
 writeQueue :: Queue a -> a -> IO ()
-writeQueue (Queue queue signal _lock) x = do
-    modifyMVar_ queue (return . (x :))
-    void (tryPutMVar signal ())
+writeQueue (Queue chan) x = writeChan chan x
 
 flushQueue :: Queue a -> IO [a]
-flushQueue (Queue queue signal lock) = withMVar lock $ \_ -> do
-    takeMVar signal
-    modifyMVar queue (\msgs -> return ([], reverse msgs))
+flushQueue (Queue chan) = return <$> readChan chan
